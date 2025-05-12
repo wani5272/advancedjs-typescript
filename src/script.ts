febe3d4b99df15152b7c58ade2f59da4dc5c45b6
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-05-12 09:33:50 +0200

    cat

--- src/script.ts
+++ src/script.ts
@@ -91,3 +91,17 @@ const todo3 = completeupdateTodo(todo1, {
 console.log(todo1);
 console.log(todo2);
 console.log(todo3);
+type CatName = "miffy" | "boris" | "mordred";
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+};
+
+cats.boris;
